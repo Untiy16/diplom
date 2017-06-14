827634fe87944d9eaa4e751600067b02307305f0
Untiy16 <untiy16@gmail.com>
2017-06-15 02:39:26 +0300

    11

--- diplom/www/js/index.js
+++ diplom/www/js/index.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
         height: contentHeight + 'px'
     });
     console.log("stage2");
-
-    
+$('#alarmdivbody').empty()
+    alarmlist = JSON.parse(localStorage.getItem("alarmliststored"));
+     $('#alarmdivbody').html(alarmlist);
+     alarmtimeparse();
+     console.log("stage5");
 
 
 });
@@ -55,7 +58,7 @@ $(document).ready(function() {
         "lesson4": "Охорона праці",
         "lesson5": "—"
     };
-
+//localStorage.setItem("sheldulestored", JSON.stringify(sheldule));
      //console.log()
 var shelduletime = [];
     console.log("stage3");
@@ -101,6 +104,94 @@ var shelduletime = [];
             $('#startless5').text(shelduletime[4].start);
             $('#finless5').text(shelduletime[4].finish);
             console.log("stage4.2");
+    }
+
+
+
+    if(JSON.parse(localStorage.getItem("sheldulestored"))==null){
+        
+        sheldule[0]={
+        "day":"Понеділок",
+        "lesson1": "",
+        "lesson2": "",
+        "lesson3": "",
+        "lesson4": "",
+        "lesson5": ""
+    };
+
+    sheldule[1]={
+        "day":"Вівторок",
+        "lesson1": "",
+        "lesson2": "",
+        "lesson3": "—",
+        "lesson4": "—",
+        "lesson5": "—"
+    };
+
+     sheldule[2]={
+        "day":"Середа",
+        "lesson1": "—",
+        "lesson2": "",
+        "lesson3": "",
+        "lesson4": "",
+        "lesson5": ""
+    };
+
+     sheldule[3]={
+        "day":"Четвер",
+        "lesson1": "",
+        "lesson2": "",
+        "lesson3": "",
+        "lesson4": "",
+        "lesson5": ""
+    };
+
+     sheldule[4]={
+        "day":"П'ятниця",
+        "lesson1": "",
+        "lesson2": "",
+        "lesson3": "",
+        "lesson4": "",
+        "lesson5": ""
+    };
+
+    }
+    else{
+        sheldule = JSON.parse(localStorage.getItem("sheldulestored"));
+            $('#day1less1').text(sheldule[0].lesson1);
+            $('#day1less2').text(sheldule[0].lesson2);
+            $('#day1less3').text(sheldule[0].lesson3);
+            $('#day1less4').text(sheldule[0].lesson4);
+            $('#day1less5').text(sheldule[0].lesson5);
+            
+            $('#day2less1').text(sheldule[1].lesson1);
+            $('#day2less2').text(sheldule[1].lesson2);
+            $('#day2less3').text(sheldule[1].lesson3);
+            $('#day2less4').text(sheldule[1].lesson4);
+            $('#day2less5').text(sheldule[1].lesson5);
+            
+            $('#day3less1').text(sheldule[2].lesson1);
+            $('#day3less2').text(sheldule[2].lesson2);
+            $('#day3less3').text(sheldule[2].lesson3);
+            $('#day3less4').text(sheldule[2].lesson4);
+            $('#day3less5').text(sheldule[2].lesson5);
+            
+            $('#day4less1').text(sheldule[3].lesson1);
+            $('#day4less2').text(sheldule[3].lesson2);
+            $('#day4less3').text(sheldule[3].lesson3);
+            $('#day4less4').text(sheldule[3].lesson4);
+            $('#day4less5').text(sheldule[3].lesson5);
+            
+            $('#day5less1').text(sheldule[4].lesson1);
+            $('#day5less2').text(sheldule[4].lesson2);
+            $('#day5less3').text(sheldule[4].lesson3);
+            $('#day5less4').text(sheldule[4].lesson4);
+            $('#day5less5').text(sheldule[4].lesson5);
+
+            
+            console.log("stage4.2");
+
+
     }
 
 
